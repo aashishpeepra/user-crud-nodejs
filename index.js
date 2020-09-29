99cedbbb0a1be26cfa903683da39734c389ea4d7
Adarsh <herecomesthebeastlesnar@gmail.com>
2020-09-29 13:07:26 +0530

    Database Connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,31 @@ const userRoutes = require("./routes/user-routes");
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH");
+
+  next();
+});
+
 app.use("/", userRoutes);
 
-app.listen(PORT);
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nhj8w.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log(`Connected to Database, Server up and running on ${PORT}`);
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
--- models/user.js
+++ models/user.js
@@ -1,1 +1,38 @@
-// Define User Schema
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  github: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  resetToken: {
+    type: String,
+    default: undefined,
+  },
+  resetTokenExpiryDate: {
+    type: Date,
+    default: undefined,
+  },
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
