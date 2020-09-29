b1f5a73cab8050af6d41136be9861b30cfd17105
Adarsh <herecomesthebeastlesnar@gmail.com>
2020-09-29 14:45:40 +0530

    Login

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,6 +1,27 @@
+const User = require("../models/user");
+const HttpError = require("../models/http-error");
+
 exports.signup = async (req, res, next) => {};
 
-exports.login = async (req, res, next) => {};
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Server Error", 501);
+    return next(error);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError("Invalid Credentials, please try again.", 404);
+    return next(error);
+  }
+  
+  res.send(existingUser);
+  
+};
 
 exports.postResetData = async (req, res, next) => {};
 
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const HttpError = require("./models/http-error");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
